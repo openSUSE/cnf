a703a6bb87c82e1cb36b9ad1044a6f0040c528b9
Michal Vyskocil <michal.vyskocil@gmail.com>
2023-10-09 23:20:45 +0200

    Problem: installed command not found handlers not tested
    Solution:
     * use handlers, which will be installed by cnf.rpm
     * add test cases for bash, zsh and fish shell

--- test/root.sh
+++ test/root.sh
@@ -11,7 +11,8 @@ set -e
 
 PROJECT_DIR=$(readlink -f "$( dirname "$BATS_TEST_FILENAME" )/..")
 CNF_SRC=$(readlink -f "${PROJECT_DIR}/target/debug/cnf")
-BASH_CNF=$(readlink -f "${PROJECT_DIR}/command_not_found_bash")
+BASH_CNF=$(readlink -f "${PROJECT_DIR}/command_not_found.bash")
+ZSH_CNF=$(readlink -f "${PROJECT_DIR}/command_not_found.zsh")
 
 USER="root"
 if [[ "${0}" =~ "nonroot.sh" ]]; then
@@ -22,21 +23,23 @@ VOLUMES=()
 VOLUMES[0]="--volume "${CNF_SRC}":/usr/bin/cnf:ro"
 # needed for fish shell: boo#1215428
 VOLUMES[1]="--volume "${CNF_SRC}":/usr/bin/command-not-found:ro"
-VOLUMES[2]="--volume "${BASH_CNF}":/etc/bash_command_not_found:ro"
+VOLUMES[2]="--volume "${BASH_CNF}":/usr/etc/bash_command_not_found:ro"
+VOLUMES[3]="--volume "${ZSH_CNF}":/usr/etc/zsh_command_not_found:ro"
 
 # github action uses the ubuntu-latest with libsolv-dev, which is a shared library
 # openSUSE distributes libsolv only as static library on the other hand
 # so if running the script on ubuntu-latest, mount the shared library into openSUSE container
 # to make it run there
 if [[ -f /usr/lib/x86_64-linux-gnu/libsolv.so.1 ]]; then
-    VOLUMES[3]="--volume /usr/lib/x86_64-linux-gnu/libsolv.so.1:/usr/lib64/libsolv.so.1:ro"
+    VOLUMES[4]="--volume /usr/lib/x86_64-linux-gnu/libsolv.so.1:/usr/lib64/libsolv.so.1:ro"
 elif [[ -f "${PROJECT_DIR}/test/libsolv.so.1" ]]; then
     # mount the libsolv.so.1 if it is in test/ directory: this supports the development
-    VOLUMES[3]="--volume "${PROJECT_DIR}/test/libsolv.so.1":/usr/lib64/libsolv.so.1:ro"
+    VOLUMES[4]="--volume "${PROJECT_DIR}/test/libsolv.so.1":/usr/lib64/libsolv.so.1:ro"
 fi
 
 docker \
     run \
+    --tty \
     --rm \
     --user "${USER}" \
     ${VOLUMES[*]} \
